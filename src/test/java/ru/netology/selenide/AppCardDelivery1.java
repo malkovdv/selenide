9320ed91b434ec870d7794cb3a37f8f9638aecf5
malkov <malkov922522@gmail.com>
2025-11-30 18:32:51 +0400

    intit

--- src/test/java/ru/netology/selenide/AppCardDelivery1.java
+++ src/test/java/ru/netology/selenide/AppCardDelivery1.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -15,26 +14,29 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class AppCardDelivery1 {
 
-@BeforeEach
-    void setup() {    Selenide.open("http://localhost:9999");}
+    @BeforeEach
+    void setup() {
+        Selenide.open("http://localhost:9999");
+    }
+
     private String generateDate(long addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
-        @Test
-        public void shouldBeSuccessfullyCompleted() {
-            $("[data-test-id='city'] input").setValue("Ижевск");
-            String planningDate = generateDate(4, "dd.MM.yyyy");
-            $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
-                    .setValue(planningDate);
-            $("[data-test-id='name'] input").setValue("Петров Дмитрий");
-            $("[data-test-id='phone'] input").setValue("+79905556617");
-            $("[data-test-id='agreement']").click();
-            $("button.button").click();
-            $(".notification__content")
-                    .should(Condition.visible, Duration.ofSeconds(15))
-                    .should(Condition.text("Встреча успешно забронирована на " + planningDate));
+    @Test
+    public void shouldBeSuccessfullyCompleted() {
+        $("[data-test-id='city'] input").setValue("Ижевск");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Петров Дмитрий");
+        $("[data-test-id='phone'] input").setValue("+79905556617");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно забронирована на " + planningDate));
 
-        }
     }
+}
 
